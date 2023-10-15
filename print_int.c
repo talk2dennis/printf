1074091774e9b66c90b5b661ead521ad11bec370
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-14 21:48:16 -0700

    created print_int.c a function that will help us print int numbers

---
+++ print_int.c
@@ -0,0 +1,16 @@
+/**
+* print_str - Function to print numbers
+* @num: int to print
+* Return: nothing
+*/
+void print_int(int num)
+{
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if (num / 10 != 0)
+		print_int(num / 10);
+	_putchar('0' + num % 10);
+}
