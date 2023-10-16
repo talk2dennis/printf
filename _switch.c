a9c90cd56427e483fb6e796f56df05d000645b93
hix.mox <hichameddarhmoum20@gmail.com>
2023-10-16 04:44:52 +0100

    pushing remove a case and change fnction

--- _switch.c
+++ _switch.c
@@ -32,8 +32,8 @@ void _switch(char c, va_list args, int *len)
 			(*len)++;
 			break;
 		case 'i':
-			case 'd':
-			_putchar(va_arg(args, int));
+		case 'd':
+			print_int(va_arg(args, int));
 			(*len)++;
 			break;
 		default:
