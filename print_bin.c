afd2d491508fa973567210e8c839ba261faf345f
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-17 07:00:58 -0700

    Revert "updated for betty"
    This reverts commit 96daf6dc4e8a2df6faf909adce72e28c54205519.

--- print_bin.c
+++ print_bin.c
@@ -3,50 +3,50 @@
 /**
  * printf_bin - print binary function
  *
- * @n: the intiger number given to convert
- * @len: the length of characters printed
+ *@n: the intiger number given to convert
  *
  * Return: nothing
  */
 void printf_bin(int n,  int *len)
 {
-	char bin_str[32];
-	int i;
+        char bin_str[32];
+        int i;
 
-	if (n < 0)
-	{
-		n = -n;
+        if (n < 0)
+        {
+                n = -n;
 
-		while (n > 0)
-		{
-			bin_str[i] = '0' + (n % 2);
-			n /= 2;
-			i++;
-		}
-		for (i = 1; i >= 0; i--)
-		{
-			_putchar('-');
-			_putchar(bin_str[i]);
-			(*len)++;
-		}
-	}
+                while (n > 0)
+                {
+                        bin_str[i] = '0' + (n % 2);
+                        n /= 2;
+                        i++;
+                }
+                for (i = 1; i >= 0; i--)
+                {
+                        _putchar('-');
+                        _putchar(bin_str[i]);
+                        (*len)++;
+                }
+        }
+
 	else if (n > 0)
 	{
-		while (n > 0)
-		{
-			bin_str[i] = '0' + (n % 2);
-			n /= 2;
-			i++;
-		}
-		for (i = i - 1; i >= 0; i--)
-		{
-			_putchar(bin_str[i]);
-			(*len)++;
-		}
-	}
-	else
+	while (n > 0)
 	{
-		_putchar('0');
-		(*len)++;
-	}
+                        bin_str[i] = '0' + (n % 2);
+                        n /= 2;
+                        i++;
+                }
+                for (i = i - 1; i >= 0; i--)
+                {
+                        _putchar(bin_str[i]);
+                        (*len)++;
+                }
+        }
+		else
+        {
+                _putchar('0');
+                (*len)++;;
+        }
 }
