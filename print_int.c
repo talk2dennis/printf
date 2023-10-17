508b824fa405fe25e7a96877a9530e2685cedfef
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-17 03:31:29 -0700

    updated for print_int

--- print_int.c
+++ print_int.c
@@ -9,16 +9,15 @@
 
 void print_int(long int num, int *len)
 {
+	(*len)++;
 	if (num < 0)
 	{
 		_putchar('-');
 		num = -num;
-		(*len)++;
 	}
 	if (num == 0)
 	{
 		_putchar('0');
-		(*len)++;
 		return;
 	}
 	if (num / 10 != 0)
@@ -26,5 +25,4 @@ void print_int(long int num, int *len)
 		print_int(num / 10, len);
 	}
 	_putchar('0' + num % 10);
-	(*len)++;
 }
