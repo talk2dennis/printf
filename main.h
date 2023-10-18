684991e656bfe9addd52e8f31674ba633d865f2b
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-18 03:55:21 +0000

    updated print_int and main.h

--- main.h
+++ main.h
@@ -8,7 +8,7 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_str(va_list args);
-void print_int(int num);
+int print_int(int num);
 int _strlen(char *str);
 void _switch(char c, va_list args, int *len);
 void printf_bin(int n,  int *len);
