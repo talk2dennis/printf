c92ed29a3627f4e86342dd09e0e04fb496243a5c
hix.mox <hichameddarhmoum20@gmail.com>
2023-10-18 19:10:22 +0100

    push betty code fixing

--- print_bin.c
+++ print_bin.c
@@ -12,19 +12,18 @@ void printf_bin(int n, int *len)
 {
 	char bin_str[32];
 	int i = 0;
-	
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	
-	do
-	{
+
+	do {
 		bin_str[i++] = '0' + (n % 2);
 		n /= 2;
 	} while (n > 0);
-	
+
 	for (i = i - 1; i >= 0; i--)
 	{
 		_putchar(bin_str[i]);
